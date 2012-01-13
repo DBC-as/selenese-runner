1afe0fbc70bec645737118f3300ec159f4e149f2
Rasmus Erik Voel Jensen <github@solsort.dk>
2012-01-13 12:22:37 +0100

    travis script for starting headless selenium

--- start-selenium.sh
+++ start-selenium.sh
@@ -1,3 +1,8 @@
 SELENIUM_JAR=selenium-server-standalone-2.15.0.jar
 test -f $SELENIUM_JAR || wget http://selenium.googlecode.com/files/$SELENIUM_JAR || exit -1
-java -jar $SELENIUM_JAR
+xvfb-run java -jar $SELENIUM_JAR >& selenium-server.log &
+
+until grep "Started org.openqa.jetty.jetty.Server" selenium-server.log; do
+    echo waiting for selenium server to start
+    sleep 1
+done
