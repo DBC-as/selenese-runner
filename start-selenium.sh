783e22d27a3f9ba8a05d08c9eb2ea83494d53693
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2012-01-13 12:27:21 +0100

    ...

--- start-selenium.sh
+++ start-selenium.sh
@@ -1,9 +1,9 @@
 SELENIUM_JAR=selenium-server-standalone-2.15.0.jar
 test -f $SELENIUM_JAR || wget http://selenium.googlecode.com/files/$SELENIUM_JAR || exit -1
 xvfb-run java -jar $SELENIUM_JAR >& selenium-server.log &
-tail -f selenium-server.log &
 
 until grep "Started org.openqa.jetty.jetty.Server" selenium-server.log; do
     echo waiting for selenium server to start
     sleep 1
 done
+tail -f selenium-server.log &
