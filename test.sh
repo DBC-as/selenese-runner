fb3bb5ffe708c638956956ed0e3fa4d2e38fe89b
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2012-01-17 23:26:23 +0100

    tests

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 export NODE_PATH=lib
-node tests/test1.js 
-echo $?
-node tests/test2.js
-echo $?
+node tests/test1.js
+test $? -eq 0 || exit 1
+node tests/test2.js 
+test $? -eq 3 || exit 1
+echo test ok
+exit 0
--- tests/test.spec.js
+++
@@ -1,8 +0,0 @@
-runner = require('seleneserunner');
-describe('selenese runner', function() {
-    it('executes a testspec, based on a config', function() {
-       runner.runWithConfig({suitelist: 'https://raw.github.com/DBC-as/selenese-runner/master/tests/data/testsuites.txt',
-            target: 'old-bibdk', url: 'http://bibliotek.dk', callback: function(arg) { console.log(arg); }});
-
-    });
-});
