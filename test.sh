8790f0a04ea854456d89b10ace1603ee3ba75f1d
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2012-01-17 23:12:12 +0100

    replace (ie. username/password/...) support + test

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 export NODE_PATH=lib
-node tests/test1.js || exit 1
-node tests/test2.js && exit 1
-echo test successfull
-exit 0
+node tests/test1.js 
+echo $?
+node tests/test2.js
+echo $?
--- tests/test1.js
+++ tests/test1.js
@@ -2,5 +2,6 @@ require('seleneserunner').runWithConfig( {
     suitelist: 'https://raw.github.com/DBC-as/selenese-runner/master/tests/data/testsuites.txt',
     target: 'old-bibdk',
     url: 'http://bibliotek.dk', 
+    replace: {AUTHOR_PETERSEN: 'petersen'},
     callback: require('seleneserunner').simpleReporter
     });
