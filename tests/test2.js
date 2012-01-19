061045365d7d5ff5c5da4c6af677485d6aaf6bfa
Rasmus Jensen <rje@guesstimate.dbc.dk>
2012-01-19 20:24:19 +0100

    -> junit in progress

--- tests/test2.js
+++ tests/test2.js
@@ -2,5 +2,5 @@ require('seleneserunner').runWithConfig( {
     suitelist: 'https://raw.github.com/DBC-as/selenese-runner/master/tests/data/testsuites-error.txt',
     target: 'old-bibdk',
     url: 'http://bibliotek.dk', 
-    callback: require('seleneserunner').simpleReporter
+    callback: require('seleneserunner').junitReporter('tests/testresult.xml')
     });
