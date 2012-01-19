b08b908dcf6c6d8fc637e7dc4c20fab6782a06a7
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2012-01-19 21:29:32 +0100

    handle already started selenium in start-selenium-script + comments

--- start-selenium.sh
+++ start-selenium.sh
@@ -1,9 +1,19 @@
 SELENIUM_JAR=selenium-server-standalone-2.15.0.jar
+
+# exit if selenium is already running
+nc localhost 4444 < /dev/null && exit
+
+# download selenium if needed
 test -f $SELENIUM_JAR || wget http://selenium.googlecode.com/files/$SELENIUM_JAR || exit -1
+
+# start selenium headless
 xvfb-run java -jar $SELENIUM_JAR >& selenium-server.log &
 
+# wait for startup
 until grep "Started org.openqa.jetty.jetty.Server" selenium-server.log; do
     echo waiting for selenium server to start
     sleep 1
 done
+
+# log to stdout
 tail -f selenium-server.log &
