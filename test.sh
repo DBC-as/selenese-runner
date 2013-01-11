5760977b4b94500468f3e1eb83ba7a54025fdaec
Rasmus Jensen <git-email@solsort.dk>
2013-01-11 11:04:10 +0100

    Unit-tests start selenium server automatically if it is not already running.

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
+# Make sure that we have a running selenium-server
+echo 'exit' | nc localhost 4444 || ./start-selenium.sh
+
 export NODE_PATH=lib
 node tests/internaltest.js || exit 1
 node tests/test1.js
