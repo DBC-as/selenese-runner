b0547a7ee520fdc9f4d36b7120c9dcce7359beba
Rasmus Jensen <git-email@solsort.dk>
2013-05-03 14:46:02 +0200

    moved repository to github.com/DBCDK/selenese-runner and left a README with link to the new url

--- tests/test1.js
+++
@@ -1,7 +0,0 @@
-require('selenese-runner').runWithConfig( {
-    suitelist: './tests/data/testsuites.txt',
-    target: 'old-bibdk',
-    url: 'http://bibliotek.dk', 
-    replace: {AUTHOR_PETERSEN: 'petersen'},
-    callback: require('selenese-runner').junitReporter('tests/testresults1.xml')
-    });
--- tests/test2.js
+++
@@ -1,6 +0,0 @@
-require('selenese-runner').runWithConfig( {
-    suitelist: './tests/data/testsuites-error.txt',
-    target: 'old-bibdk',
-    url: 'http://bibliotek.dk', 
-    callback: require('selenese-runner').junitReporter('tests/testresult.xml')
-    });
